745aa6f16b258318d266995bf6f3289e064df24e
jalondonh@unal.edu.co <jalondonh@unal.edu.co>
2019-06-05 13:04:27 -0500

    lab 02 commit jalondonh

--- script.sh
+++ script.sh
@@ -1,1 +1,20 @@
+#!/bin/bash
 # Escriba su código aquí
+for i in data*;do sed '/^$/d' $i > out1$i;done
+for i in out1data*; do csvstack -l -H $i > out2$i; done
+csvstack -g data1.csv,data2.csv,data3.csv out2* > data.csv
+sed '1d' data.csv > out
+awk '{gsub(/\t/,",");gsub(/[[:space:]]+/,",");print}' out > data.csv
+N="$(grep -o -E '[a-z]+:[0-9]' data.csv | wc -l)"
+
+for i in `seq $(echo "$N")`
+do
+    for i in $(cat data.csv)
+    do
+        echo $i | awk '{print gensub(/([a-z]+[0-9]\.[a-z]+,[0-9],[A-Z])(.+)(,[a-z]+:[0-9])/,"\\1\\2""\n\\1\\3",1)}' >> temp
+    done  < data.csv
+    #rm data.csv
+    mv temp data.csv
+done
+cat data.csv
+rm data.csv| rm out*
